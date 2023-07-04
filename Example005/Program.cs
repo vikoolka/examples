1433f058848efa6cfdb9c2cf6b7f8cd9a76719eb
Vika <vir.89@yandex.ru>
2023-07-04 14:00:39 +0500

    Example 005 правка Маши)

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if (username.ToLower() == "Маша")
+if (username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
